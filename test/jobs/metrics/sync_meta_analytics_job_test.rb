14faca51edb9a84cc68428a0b64160ef88d45b9f
xrendan <brendan@brendansamek.com>
2026-08-13 08:52:41 -0600

    Remove custom Meta enqueue error handling

--- test/jobs/metrics/sync_meta_analytics_job_test.rb
+++ test/jobs/metrics/sync_meta_analytics_job_test.rb
@@ -1,12 +1,6 @@
 require "test_helper"
 
 class Metrics::SyncMetaAnalyticsJobTest < ActiveJob::TestCase
-  class FailingRetryJob
-    def self.perform_later(*)
-      raise ActiveJob::EnqueueError, "queue unavailable"
-    end
-  end
-
   class TestJob < Metrics::SyncMetaAnalyticsJob
     attr_reader :attempted
 
@@ -50,14 +44,6 @@ class Metrics::SyncMetaAnalyticsJobTest < ActiveJob::TestCase
     end
   end
 
-  class QueueFailureTestJob < TestJob
-    private
-
-    def retry_job_class
-      FailingRetryJob
-    end
-  end
-
   test "isolates failed accounts and continues syncing later accounts" do
     job = TestJob.new
 
@@ -67,14 +53,6 @@ class Metrics::SyncMetaAnalyticsJobTest < ActiveJob::TestCase
     ) do
       job.perform_now
     end
-
-    assert_equal %w[build_canada build_toronto], job.attempted
-  end
-
-  test "continues syncing when enqueueing an account retry fails" do
-    job = QueueFailureTestJob.new
-
-    assert_nothing_raised { job.perform_now }
 
     assert_equal %w[build_canada build_toronto], job.attempted
   end
